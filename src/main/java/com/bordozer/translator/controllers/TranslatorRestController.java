097215dbd289fcf14ff9f3ccf0bf4c864d9b4333
Borys Lukianov <blukianov@corelogic.com>
2017-11-01 17:32:21 +0200

    Translation service: 6 - translations reloagding endpoint

--- src/main/java/com/bordozer/translator/controllers/TranslatorRestController.java
+++ src/main/java/com/bordozer/translator/controllers/TranslatorRestController.java
@@ -1,5 +1,8 @@
 package com.bordozer.translator.controllers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,28 +15,48 @@ import com.bordozer.translator.service.TranslatorService;
 import com.google.common.collect.Maps;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/translate")
 @RequiredArgsConstructor
 public class TranslatorRestController {
 
     private final TranslatorService translatorService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public TranslationDTO translateMultiple(final ModifiableTranslationDTO translations, final Language language) {
+    @RequestMapping(method = RequestMethod.GET, value = "/translate/")
+    public ResponseEntity<TranslationDTO> translateMultiple(final ModifiableTranslationDTO translations,
+        final Language language) {
         assert language != null;
         log.info("Translate multiple, language: {}, nerds map: {}", language, translations);
-        return new ImmutableTranslationDTO.Builder()
-            .translations(Maps.transformValues(translations.getTranslations(), nerd -> translateSingle(nerd, language)))
-            .build();
+
+        return new ResponseEntity<>(
+            new ImmutableTranslationDTO.Builder()
+                .translations(
+                    Maps.transformValues(translations.getTranslations(), nerd -> translateSingle(nerd, language))
+                )
+                .build(), getHttpHeaders(), HttpStatus.OK);
+    }
+
+    @SneakyThrows
+    @RequestMapping(method = RequestMethod.GET, value = "/reload")
+    public ResponseEntity<String> reload() {
+        log.info("Translation reloaded by request");
+        translatorService.reloadTranslations();
+        return new ResponseEntity<>("Done", getHttpHeaders(), HttpStatus.OK);
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Access-Control-Allow-Origin", "*");
+        headers.set("Access-Control-Allow-Headers", "X-Requested-With");
+        headers.set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+        return headers;
     }
 
     private String translateSingle(final String text, final Language language) {
         assert language != null;
-//        log.info("Translate single value: {}", text);
         return translatorService.translate(text, language);
     }
 }
