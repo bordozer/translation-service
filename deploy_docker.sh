7c362b6b4614ea405263970a07be3c3f254b88ee
Borys Lukianov <blukianov@smartling.com>
2019-11-04 16:32:08 +0200

    DOCKER: Dockerfile

---
+++ deploy_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker image build -t translation-service-docker:1.1 .
+docker container run --publish 8978:8977 --name translator-service translation-service-docker:1.1
+
+#8080:8080
+#8978:8977
+#docker run --publish 8978:8977 --name translator-service -t com.bordozer/translation-service-docker
+#docker run --publish 8080:8080 --detach --name tsd -t com.bordozer/translation-service-docker
+#docker container rm --force tsd
