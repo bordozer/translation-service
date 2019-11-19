fee3ecde3ba93914f8680ac20bc55de7b6a90c1b
Borys Lukianov <blukianov@smartling.com>
2019-11-19 14:57:16 +0200

    DOCKER: ip 2

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -6,8 +6,10 @@ docker image build -t img.translator.bordozer:1.1 .
 
 docker container run \
     --rm \
-    --network=multi-host-network \
-    --ip 192.168.0.10 \
+    --network=bordozer-network \
     --publish 8978:8977 \
     --name translator.bordozer \
     img.translator.bordozer:1.1
+
+#    --network=bordozer-network \
+#    --ip 192.168.0.10 \
