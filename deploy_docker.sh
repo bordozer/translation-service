9be87a510fc0ea3494fa99d73e22f91983f72e8e
Borys Lukianov <blukianov@smartling.com>
2019-11-19 14:05:23 +0200

    DOCKER: namespace

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-echo ""
-echo "[ Killing translator-service container ]"
-docker container rm --force translator-service
-
-echo ""
-echo "[ Gradle build ]"
 ./gradlew clean build -x check
 
-echo ""
-echo "[ Create docker immage ]"
-docker image build -t translation-service:1.1 .
+docker image build -t img.translator.bordozer:1.1 .
 
-echo ""
-echo "[ Publish docker image ]"
-docker container run --publish 8978:8977 --name translator-service translation-service:1.1
-
-#docker ps
-#docker container rm --force translator-service
-
-#docker run --publish 8978:8977 --name translator-service -t com.bordozer/translation-service
-#docker run --publish 8080:8080 --detach --name tsd -t com.bordozer/translation-service
-#-e "SPRING_PROFILES_ACTIVE=dev"
-
-
-#docker build -f Dockerfile -t micro-boot .
-#docker image ls -a
+docker container run \
+   --rm \
+   --publish 8978:8977 \
+   --name translator.bordozer \
+   img.translator.bordozer:1.1
