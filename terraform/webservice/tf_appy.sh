4200772f96b5b0522b114296262f48a32cd5f5ee
Borys Lukianov <blukianov@smartling.com>
2020-02-07 18:36:49 +0200

    Terraform conquest: add env to remote state

--- terraform/webservice/tf_appy.sh
+++ terraform/webservice/tf_appy.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
+SERVICE_NAME="translator-service"
+
 # env: `stage` or `prod`
 ENV=$1
 
 # show terraform version
 terraform -version
 
-terraform init
+terraform init \
+  -backend-config="key=${SERVICE_NAME}.${ENV}.terraform.tfstate"
 
 terraform apply -var-file=env/${ENV}.tfvars -auto-approve
-
-#  -backend-config="bucket=${REMOTE_STATES_BUCKET}" \
-#  -backend-config="key=${SERVICE_NAME}.${ENV}" \
-#  -backend-config="encrypt=true" \
-#  -backend-config="kms_key_id=${KMS_KEY_ID}" \
-#  -backend-config="region=us-east-1" \
-#  -backend-config="dynamodb_table=${TERRAFORM_STATE_LOCKS_TABLE_NAME}" \
-#  -backend=true \
-#  -auto-approve
 
 # terraform init -reconfigure
 # terraform fmt
@@ -25,3 +19,4 @@ terraform apply -var-file=env/${ENV}.tfvars -auto-approve
 # terraform plan
 # terraform apply
 # terraform destroy
+# terraform state list
--- terraform/webservice/variables.tf
+++ terraform/webservice/variables.tf
@@ -11,12 +11,11 @@ variable "ec2_deployment_minimum_healthy_percent" { default = 0 }
 variable "ec2_health_check_grace_period_seconds" { default = "60" }
 
 variable "remote_state_s3_bucket" { default = "arn:aws:s3:::remote-state-bucket" }
-//variable "remote_state_s3_key" {}
 variable "remote_state_kms_key_id" { default = "arn:aws:s3:::elasticbeanstalk-us-west-2-899415655760" }
 
 variable "docker_image_tag" { default = "latest" }
-variable "eureka_server_dns_name" { default = "" } /* TODO */
 variable "docker_registry" { default = "" } /* TODO */
+variable "eureka_server_dns_name" { default = "" } /* TODO */
 
 variable "app_java_opts_mem" { default = "-Xmx512m -Xms512m" }
 variable "app_container_mem_limit_mb" { default = 300 }
