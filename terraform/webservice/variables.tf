a88e3695957829d6a5f0f36bc93281d9fcdbaaaf
Borys Lukianov <blukianov@smartling.com>
2020-02-12 18:20:29 +0200

    TF - deployed

--- terraform/webservice/variables.tf
+++ terraform/webservice/variables.tf
@@ -45,4 +45,4 @@ variable "app_container_cpu_limit_units" { default = 200 }
 /* TODO: provide pub file as parameter */
 variable "ssh_public_key_file_path" { default = "/home/blu/.ssh/aws/vgn-pub-key.pub" }
 
-variable "application_port" { default = 8977 }
+variable "application_port" { default = 80 }
