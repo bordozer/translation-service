e75c7583d5dfb4bdb269278a064f5d8d5854126f
Borys Lukianov <blukianov@smartling.com>
2020-02-14 13:43:19 +0200

    TF - deploy script

--- scripts/deploy_aws.sh
+++ scripts/deploy_aws.sh
@@ -8,7 +8,13 @@ echo "Copy jar to EC2 instance"
 scp \
     -i ~/.ssh/aws/vgn-pub-key.pem \
     ../build/libs/translation-service-1.2.jar \
-    ${EC2_USER}@${EC2_HOST}
+    "${EC2_USER}@${EC2_HOST}":/
+
+scp \
+  -i ~/.ssh/aws/vgn-pub-key.pem \
+  ../build/libs/translation-service-1.2.jar \
+  ec2-user@ec2-15-188-77-120.eu-west-3.compute.amazonaws.com:/home/ec2-user/
+
 
 echo "Logging to EC2 instance"
 ssh -i ~/.ssh/aws/vgn-pub-key.pem "${EC2_USER}@${EC2_HOST}"
@@ -20,18 +26,24 @@ tar xvzf ${JAVA_ZIP_NAME}
 mv ${JAVA_ZIP_NAME} java8
 export JAVA_HOME=~/java8
 
-echo "Downloading gradle"
-wget "https://services.gradle.org/distributions/gradle-4.10.2-bin.zip"
-unzip gradle-4.10.2-bin.zip
-mv gradle-4.10.2 gradle
-export GRADLE_HOME=~/gradle
+#echo "Downloading gradle"
+#wget "https://services.gradle.org/distributions/gradle-4.10.2-bin.zip"
+#unzip gradle-4.10.2-bin.zip
+#mv gradle-4.10.2 gradle
+#export GRADLE_HOME=~/gradle
+#export PATH=$JAVA_HOME/bin:$GRADLE_HOME/bin:$PATH
 
-export PATH=$JAVA_HOME/bin:$GRADLE_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$PATH
 
 echo "Deploy jar"
-gradle clean build -x test && java -ea -Dspring.profiles.active=local -jar build/libs/translation-service-1.1.jar
+#fuser -k 8977/tcp
+kill -9 "$(lsof -t -i:8977)"
+java -ea -Dspring.profiles.active=local -jar build/libs/translation-service-1.1.jar
 
-
+#scp \
+#  -i ~/.ssh/aws/vgn-pub-key.pem \
+#  ../build/libs/translation-service-1.2.jar \
+#  ec2-user@ec2-15-188-77-120.eu-west-3.compute.amazonaws.com:/
 
 #ssh -i ~/.ssh/aws/vgn-pub-key.pem "ec2-user@ec2-15-188-77-120.eu-west-3.compute.amazonaws.com"
 #tar xvzf amazon-corretto-8-aarch64-linux-jdk.tar.gz
