97b70073e99b6086ff728bf01448bedafa62d259
Borys Lukianov <blukianov@smartling.com>
2020-02-17 15:20:38 +0200

    Installing java on EC2 in user data 5

--- scripts/aws_ec2_instance_dns.sh
+++ scripts/aws_ec2_instance_dns.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#dns=''
+
+#function extract_dns {
+#  str="$1"
+#  IFS='"'
+#  echo "======$str"
+#  readarray -d : -t strarr <<< "$str"
+#  dns=${strarr[1]}
+#}
+
 SERVICE_EC2_TAG=$1
 if [ -z "$SERVICE_EC2_TAG" ]
 then
@@ -7,15 +17,18 @@ then
       exit 1;
 fi
 
-EC2_INSTANCE_ID=$(./aws_ec2_instance_id.sh $SERVICE_EC2_TAG)
+EC2_INSTANCE_ID=$(./aws_ec2_instance_id.sh "$SERVICE_EC2_TAG")
 if [ -z "$EC2_INSTANCE_ID" ]
 then
       echo "Cannot get EC2 instance ID for tag '$SERVICE_EC2_TAG'"
       exit 1;
 fi
 
-query_result=$(aws ec2 describe-instances --instance-ids $EC2_INSTANCE_ID --query "Reservations[].Instances[].PublicDnsName")
+query_result=$(aws ec2 describe-instances --instance-ids "$EC2_INSTANCE_ID" --query "Reservations[].Instances[].PublicDnsName")
 
-dns="${query_result:7:48}"
+#pat='"(.*?)"'
+#[[ "$query_result" =~ $pat ]]
+#echo "${BASH_REMATCH[0]}"
 
-echo "$dns";
+dns=$(grep -o '".*"' "$query_result" | sed 's/"//g')
+echo $dns
