52f2db6213bb9c6b67228d8b597b65e5b838293f
Borys Lukianov <blukianov@smartling.com>
2020-02-17 17:23:02 +0200

    Add s3.tf to git

---
+++ terraform/webservice/s3.tf
@@ -0,0 +1,5 @@
+resource "aws_s3_bucket_object" "artefact_upload" {
+  bucket = "${var.app_artefacts_s3_bucket}"
+  key = "tf-${var.service_name}.jar"
+  source = "../../build/libs/${var.app_artefact_name}"
+}
--- terraform/webservice/user_data.sh
+++ terraform/webservice/user_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# install software
 sudo yum install mc -y
-
 sudo yum install java-1.8.0 -y
+sudo yum install awscli -y
 
+# get EC2 vars
 ec2_instance_id=$(ec2-metadata -i | cut -d ' ' -f2)
 ec2_region=$(ec2-metadata --availability-zone | cut -d ' ' -f2)
 ec2_region=${ec2_region%?}
@@ -13,15 +15,26 @@ ec2_app_artefact_s3_bucket=$(aws ec2 describe-tags --filters "Name=resource-id,V
 app_env=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$ec2_instance_id" "Name=key,Values=Environment" --region "$ec2_region" --output=text | cut -f5)
 app_artefact_name=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$ec2_instance_id" "Name=key,Values=AppArtefactFileName" --region "$ec2_region" --output=text | cut -f5)
 
+# set env vars
 echo "export JAVA_HOME=/usr/bin/java" >> ~/.bashrc
 
 echo "" >> ~/.bashrc
 echo "export EC2_INSTANCE_ID='$ec2_instance_id'" >> ~/.bashrc
 echo "export EC2_REGION='$ec2_region'" >> ~/.bashrc
 echo "export EC2_SERVICE_NAME='${ec2_service_name}'" >> ~/.bashrc
-echo "export EC2_APP_ARTEFACT_S3_BUCKET='${ec2_app_artefact_s3_bucket}'" >> ~/.bashrc
 
 echo "" >> ~/.bashrc
 echo "export EC2_APP_ENV='${app_env}'" >> ~/.bashrc
 
-aws s3 cp "${app_artefact_name}" "s3://${ec2_app_artefact_s3_bucket}"
+# get app jar from S3
+mkdir -p ~/app
+cd app
+aws s3 sync "s3://${ec2_app_artefact_s3_bucket}" ~/app
+
+sudo mkdir -p "/var/log/bordozer/${ec2_service_name}/"
+sudo chmod 777 -R "/var/log/bordozer/${ec2_service_name}/"
+
+# run app
+$JAVA_HOME/bin/java -ea -Dspring.profiles.active=aws -jar "${app_artefact_name}"
+
+#aws s3 sync "s3://artefacts-s3-bucket" ~/app
