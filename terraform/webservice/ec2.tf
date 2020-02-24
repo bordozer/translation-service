1c450d82112f30ac787ef9ec1f2cca279dfc11b7
Borys Lukianov <blukianov@smartling.com>
2020-02-24 13:10:41 +0200

    Tune user data script 2

--- terraform/webservice/ec2.tf
+++ terraform/webservice/ec2.tf
@@ -38,6 +38,5 @@ data "template_file" "ec2_userdata" {
     t_app_dir = "/opt/${var.service_instance_name}"
     t_app_artefact_s3_bucket = var.app_artefacts_s3_bucket
     t_app_artefact_name = "tf-${var.service_instance_name}"
-    t_app_artefact_file = "tf-${var.service_instance_name}.jar"
   }
 }
