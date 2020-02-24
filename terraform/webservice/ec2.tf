8910232ca47eb77c6ce48067a187fa262d04a779
Borys Lukianov <blukianov@smartling.com>
2020-02-24 13:07:02 +0200

    Tune user data script

--- terraform/webservice/ec2.tf
+++ terraform/webservice/ec2.tf
@@ -34,8 +34,10 @@ data "template_file" "ec2_userdata" {
   vars = {
     t_service_name = var.service_name
     t_service_instance_name = var.service_instance_name
-    t_app_artefact_s3_bucket = var.app_artefacts_s3_bucket
-    t_app_artefact_name = "tf-${var.service_instance_name}.jar"
+    t_env = var.environment_name
     t_app_dir = "/opt/${var.service_instance_name}"
+    t_app_artefact_s3_bucket = var.app_artefacts_s3_bucket
+    t_app_artefact_name = "tf-${var.service_instance_name}"
+    t_app_artefact_file = "tf-${var.service_instance_name}.jar"
   }
 }
--- terraform/webservice/user_data.sh
+++ terraform/webservice/user_data.sh
@@ -7,20 +7,20 @@ yum install java-1.8.0 -y
 yum install awscli -y
 
 # create log dirs
-sudo mkdir -p "/var/log/bordozer/${t_service_name}/"
-sudo chmod 777 -R "/var/log/bordozer/${t_service_name}/"
+mkdir "${t_app_dir}"
+mkdir -p "/var/log/bordozer/${t_service_name}/"
+chmod 777 -R "/var/log/bordozer/${t_service_name}/"
 
 # Get app artefact
-mkdir "${t_app_dir}"
-echo "RUN_ARGS='--spring.profiles.active=aws'" >"${t_app_dir}/${t_app_artefact_name}.conf"
-aws s3 cp "s3://${t_app_artefact_s3_bucket}/${t_app_artefact_name}" "${t_app_dir}/"
+echo "RUN_ARGS='--spring.profiles.active=aws-${t_env}'" >"${t_app_dir}/${t_app_artefact_name}.conf"
+aws s3 cp "s3://${t_app_artefact_s3_bucket}/${t_app_artefact_file}" "${t_app_dir}/"
 
 useradd springboot
 chsh -s /sbin/nologin springboot
-chown springboot:springboot "${t_app_dir}/${t_app_artefact_name}"
-chmod 500 "${t_app_dir}/${t_app_artefact_name}"
+chown springboot:springboot "${t_app_dir}/${t_app_artefact_file}"
+chmod 500 "${t_app_dir}/${t_app_artefact_file}"
 
-ln -s "${t_app_dir}/${t_app_artefact_name}" "/etc/init.d/${t_service_instance_name}"
+ln -s "${t_app_dir}/${t_app_artefact_file}" "/etc/init.d/${t_service_instance_name}"
 
 chkconfig "${t_service_instance_name}" on
 service "${t_service_instance_name}" start
@@ -28,6 +28,6 @@ service "${t_service_instance_name}" start
 # aws s3 cp "s3://artefacts-s3-bucket/tf-translator-service-staging.jar" "/opt/translator-service-staging"
 # chown springboot:springboot "/opt/translator-service-staging/tf-translator-service-staging.jar"
 # chmod 500 "/opt/translator-service-staging/tf-translator-service-staging.jar"
-# rm "/etc/init.d/translator-service-staging"
-# ln -s "/opt/translator-service-staging/tf-translator-service-staging.jar" "/etc/init.d/translator-service-staging"
-# service "translator-service-staging" start
+# sudo rm "/etc/init.d/translator-service-staging"
+# sudo ln -s "/opt/translator-service-staging/tf-translator-service-staging.jar" "/etc/init.d/translator-service-staging"
+# sudo service "translator-service-staging" start
