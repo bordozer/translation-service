95a23cb94ac5a9b59a742d813f4284e7fd61029b
Borys Lukianov <blukianov@smartling.com>
2020-03-02 18:10:37 +0200

    ASG policy

--- terraform/drafts/asg_policy.tf
+++ terraform/drafts/asg_policy.tf
@@ -1,9 +1,17 @@
 resource "aws_autoscaling_policy" "heavy_out" {
-  name = "heavy-out-asg-policy"
+  name = "tf-${var.service_instance_name}-asg-add-policy"
   scaling_adjustment = 1
   adjustment_type = "ChangeInCapacity"
   cooldown = 10
-  autoscaling_group_name = "${aws_autoscaling_group.heavy.name}"
+  autoscaling_group_name = aws_autoscaling_group.service_asg.name
+}
+
+resource "aws_autoscaling_policy" "heavy-in" {
+  name = "tf-${var.service_instance_name}-asg-remove-policy"
+  scaling_adjustment = -1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 10
+  autoscaling_group_name = aws_autoscaling_group.service_asg.name
 }
 
 resource "aws_cloudwatch_metric_alarm" "heavy_asg_cpu_usage_is_very_high" {
@@ -16,8 +24,26 @@ resource "aws_cloudwatch_metric_alarm" "heavy_asg_cpu_usage_is_very_high" {
   statistic = "Average"
   threshold = 80
   dimensions {
-    AutoScalingGroupName = "${aws_autoscaling_group.heavy.name}"
+    AutoScalingGroupName = aws_autoscaling_group.service_asg.name
   }
   alarm_description = "This metric monitors EC2 CPU utilization"
-  alarm_actions = ["${aws_autoscaling_policy.heavy_out.arn}"]
+  alarm_actions = [
+    aws_autoscaling_policy.heavy_out.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "heavy_asg_cpu_usage_is_very_low" {
+  alarm_name = "heavy-asg-cpu-usage-is-very-low"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods = 1
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = 60
+  statistic = "Average"
+  threshold = 40
+  dimensions {
+    AutoScalingGroupName = aws_autoscaling_group.service_asg.name
+  }
+  alarm_description = "This metric monitors EC2 CPU utilization"
+  alarm_actions = [
+    aws_autoscaling_policy.heavy-in.arn]
 }
