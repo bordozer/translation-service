34729a3b9e885da03b019589b7317ce4138ff79a
Borys Lukianov <blukianov@smartling.com>
2020-03-02 11:04:15 +0200

    TF - ALB access logs

--- terraform/webservice/lb.tf
+++ terraform/webservice/lb.tf
@@ -2,14 +2,16 @@ resource "aws_lb" "front_end" {
   name = "tf-${var.service_instance_name}-lb"
   load_balancer_type = "application"
   subnets = var.subnets
-  security_groups = [ "${aws_security_group.lb_sg.id}" ]
+  security_groups = [ aws_security_group.lb_sg.id ]
 
   internal = var.internal_lb_scheme
   enable_cross_zone_load_balancing = true
   idle_timeout = 60
 
+//  enable_deletion_protection = true
+
   /*access_logs {
-    bucket = "${var.lb_access_logs_s3_bucket}"
+    bucket = var.lb_access_logs_s3_bucket
     prefix = "tf-${var.service_instance_name}-logs"
     enabled = true
   }*/
@@ -17,16 +19,17 @@ resource "aws_lb" "front_end" {
   tags = {
     Name = var.service_instance_name
     Environment = var.environment_name
+    Description = "Translator API ALB"
   }
 }
 
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = "${aws_lb.front_end.arn}"
+  load_balancer_arn = aws_lb.front_end.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.lb_tg.arn}"
+    target_group_arn = aws_lb_target_group.lb_tg.arn
   }
 }
--- terraform/webservice/variables.tf
+++ terraform/webservice/variables.tf
@@ -23,7 +23,7 @@ variable "subnets" {
   ]
 }
 variable "route53_zone_id" { default = "ZYQ37WWIE7SAZ" }
-variable "lb_access_logs_s3_bucket" { default = "lb_access_logs" }
+variable "lb_access_logs_s3_bucket" { default = "app-lb-access-logs" }
 
 /* EC2 parameters */
 variable "instance_type" { default = "t2.micro" }
