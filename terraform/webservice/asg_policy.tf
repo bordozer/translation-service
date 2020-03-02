db751d460f60722d15d372b3e583a4555e2d9e0c
Borys Lukianov <blukianov@smartling.com>
2020-03-02 19:16:10 +0200

    ASG policies + alarms tuning

--- terraform/webservice/asg_policy.tf
+++ terraform/webservice/asg_policy.tf
@@ -1,21 +1,22 @@
-resource "aws_cloudwatch_metric_alarm" "heavy_asg_cpu_usage_is_very_high" {
+resource "aws_cloudwatch_metric_alarm" "cpu_usage_is_very_high" {
   alarm_name = "tf-${var.service_instance_name}-CPU-too-high"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods = 3 # The number of periods over which data is compared to the specified threshold.
   metric_name = "CPUUtilization"
   namespace = "AWS/EC2"
-  period = 60
+  period = 60 # The period in seconds over which the specified statistic is applied
   statistic = "Average"
-  threshold = 80
+  threshold = 80 # The value against which the specified statistic is compared
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.service_asg.name
   }
   alarm_description = "Add instance if CPU Utilization is too high"
   alarm_actions = [
-    aws_autoscaling_policy.heavy_out.arn]
+    aws_autoscaling_policy.add_instance_policy.arn
+  ]
 }
 
-resource "aws_autoscaling_policy" "heavy_out" {
+resource "aws_autoscaling_policy" "add_instance_policy" {
   name = "tf-${var.service_instance_name}-asg-add-instance-policy"
   scaling_adjustment = 1
   adjustment_type = "ChangeInCapacity"
@@ -24,8 +25,8 @@ resource "aws_autoscaling_policy" "heavy_out" {
   policy_type = "SimpleScaling"
 }
 
-resource "aws_cloudwatch_metric_alarm" "heavy_asg_cpu_usage_is_very_low" {
-  alarm_name = "tf-${var.service_instance_name}-CPU-is-low"
+resource "aws_cloudwatch_metric_alarm" "cpu_usage_is_very_low" {
+  alarm_name = "tf-${var.service_instance_name}-CPU-is-too-low"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods = 1
   metric_name = "CPUUtilization"
@@ -36,12 +37,13 @@ resource "aws_cloudwatch_metric_alarm" "heavy_asg_cpu_usage_is_very_low" {
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.service_asg.name
   }
-  alarm_description = "Remove instance if CPU Utilization is ok"
+  alarm_description = "Remove instance if CPU Utilization is too low"
   alarm_actions = [
-    aws_autoscaling_policy.heavy-in.arn]
+    aws_autoscaling_policy.remove_instance_policy.arn
+  ]
 }
 
-resource "aws_autoscaling_policy" "heavy-in" {
+resource "aws_autoscaling_policy" "remove_instance_policy" {
   name = "tf-${var.service_instance_name}-asg-remove-instance-policy"
   scaling_adjustment = -1
   adjustment_type = "ChangeInCapacity"
@@ -49,3 +51,5 @@ resource "aws_autoscaling_policy" "heavy-in" {
   autoscaling_group_name = aws_autoscaling_group.service_asg.name
   policy_type = "SimpleScaling"
 }
+
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/aws-services-cloudwatch-metrics.html
